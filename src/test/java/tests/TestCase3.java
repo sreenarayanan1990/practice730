9680dfbffe565cd0d7463a828f0f85b01d80ade7
SREENARAYANAN <sreenarayanan@example.com>
2025-05-09 18:31:04 +0530

    Removed unwanted files from tracking

--- src/test/java/tests/TestCase3.java
+++ src/test/java/tests/TestCase3.java
@@ -29,7 +29,8 @@ public class TestCase3 {
 		BrowserUtils.KillBrowser();
 		report.endTest(test);
 		ReportUtils.UpdateAndCloseReport();
-		ReportUtils.launchReport();		
+		ReportUtils.launchReport();
+		ReportUtils.launchReport();	
 		
 	}
 }
