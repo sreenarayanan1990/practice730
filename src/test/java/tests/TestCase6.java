e5a123122822c01a92f345abb53b8a8cc4329884
SREENARAYANAN <sreenarayanan@example.com>
2025-05-10 13:32:27 +0530

    added 1 file

---
+++ src/test/java/tests/TestCase6.java
@@ -0,0 +1,38 @@
+package tests;
+
+import static app.Common.report;
+import static app.Common.test;
+
+import org.junit.Test;
+
+import pages.ResultsPage;
+import pages.SearchPage;
+import utils.BrowserUtils;
+import utils.FrameWorkUtils;
+import utils.ReportUtils;
+
+public class TestCase6 {
+	
+
+	@Test
+	public void test() throws Exception {
+		
+		SearchPage searchPage = new SearchPage();
+		ResultsPage resultsPage = new ResultsPage();
+		ReportUtils.setupReport();
+		test = report.startTest("test case 01");
+		FrameWorkUtils.setupDriver();
+		BrowserUtils.launchBrowser();
+		BrowserUtils.invokeApp();
+		searchPage.perfromSearch();
+		resultsPage.VerifySearch();
+		BrowserUtils.KillBrowser();
+		report.endTest(test);
+		ReportUtils.UpdateAndCloseReport();
+		ReportUtils.launchReport();
+		ReportUtils.launchReport();
+		ReportUtils.launchReport();
+		ReportUtils.launchReport();
+		
+	}
+}
